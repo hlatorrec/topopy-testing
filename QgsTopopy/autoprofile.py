c490f3e34c789b7abcb228b8e50536c181ad3ca7
hlatorrec <105760925+hlatorrec@users.noreply.github.com>
2022-06-08 12:56:12 +0200

    test

--- QgsTopopy/autoprofile.py
+++ QgsTopopy/autoprofile.py
@@ -16,7 +16,7 @@ def get_slope_rval(x, y):
 
 
 def fit_channel(dist, height, side_pts):
-    if side_pts < 5:
+    if side_pts < 10:
         print('Too few side points for local fits')
         return 0, 0
     elif side_pts > height.size/2 - 1:
@@ -59,6 +59,28 @@ def find_main_slopes(dist, height):
 
     crossings = find_crossings(rvalues, np.mean(rvalues))
     dist_index = crossings + side_pts
+    #Inserto el cero y el final para los tramos de recta
+    dist_index = np.append(dist_index, dist.size-1)
+    dist_index = np.insert(dist_index, 0, 0)
+    #Faltaría considerar el caso en el que rvalues[0] > rmean
+    #Eso solo pasa si side_pts es demasiado pequeño
+    #o si el canal no tiene un primer tramo lo sificientemente largo
+    sections = []
+    i = 0
+    while i < 4:
+        start = dist_index[i]
+        end = dist_index[i+1]
+        x = dist[start:end]
+        z = height[start:end]
+        linreg = linregress(x, z)
+        #Basta con guardar pendiente, cte y los puntos para saber hasta dónde usar cada recta
+        recta = (linreg.slope, linreg.intercept, dist[start], dist[end])
+        sections.append(recta)
+        i+=2
+        
+    return sections
+        
+    
     
     
 
@@ -68,8 +90,17 @@ channel = np.loadtxt('channel.txt')
 z = np.trim_zeros(channel, 'b')
 x = np.arange(0, z.size, 1)
 
+rectas = find_main_slopes(x, z)
+der = np.gradient(z, x)
+plt.plot(x,der)
+for recta in rectas:
+    rx = x[recta[2] : recta[3]]
+    ry = recta[0]*np.ones(rx.size)
+    plt.plot(rx,ry)
+
 #linreg = linregress(x, z)
 
+'''
 side_pts = 10
 fit_x = x[side_pts : x.size - side_pts]
 slopes, rvalues = fit_channel(x, z, side_pts)
@@ -82,6 +113,7 @@ plt.plot(fit_x, rvalues)
 plt.plot(fit_x, rmean)
 plt.plot(fit_x, rmedian)
 crossings = find_crossings(rvalues, rmedian) #Para encontrar cambios de signo
+'''
 
 #pts_range = (10, 80)
 #means, medians = meanmed_compare(x, z, pts_range)
