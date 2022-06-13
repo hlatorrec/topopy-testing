5227aa9b09db8256d8254cc8cdb340a43529898c
hlatorrec <105760925+hlatorrec@users.noreply.github.com>
2022-06-13 13:09:27 +0200

    test

--- QgsTopopy/get_network.py
+++ QgsTopopy/get_network.py
@@ -1,25 +1,24 @@
-# -*- coding: utf-8 -*-
 from qgis.PyQt.QtCore import QCoreApplication
-from qgis.core import (QgsProcessing,
-                       QgsFeatureSink,
-                       QgsProcessingException,
-                       QgsProcessingAlgorithm,
-                       QgsProcessingParameterFeatureSource,
-                       QgsProcessingParameterFeatureSink)
+from qgis.core import (QgsProcessing, QgsProcessingException, QgsFeatureSink, QgsProcessingAlgorithm,
+                       QgsProcessingParameterRasterLayer, QgsProcessingParameterFileDestination,
+                       QgsProcessingParameterNumber, QgsProcessingParameterBoolean)
 from qgis import processing
 
-from topopy import Network
+from topopy import Flow, Network
 
 
 class GetNetwork(QgsProcessingAlgorithm):
     INPUT_FD = 'INPUT_FD'
     OUTPUT_NETW = 'OUTPUT_NETW'
+    THRESHOLD = 'THRESHOLD'
+    THETAREF = 'THETAREF'
+    GRADIENTS = 'GRADIENTS'
 
     def tr(self, string):
         return QCoreApplication.translate('getnetwork', string)
 
     def createInstance(self):
-        return ExampleProcessingAlgorithm()
+        return type(self)()
 
     def name(self):
         return 'getnetwork'
@@ -37,86 +36,21 @@ class GetNetwork(QgsProcessingAlgorithm):
         return self.tr("")
 
     def initAlgorithm(self, config=None):
-        self.AddParameter(QgsProcessingParameterRasterLayer(self.INPUT_FD, self.tr('Input Flow Direction raster')))
+        self.addParameter(QgsProcessingParameterRasterLayer(self.INPUT_FD, self.tr('Input Flow Direction raster')))
+        self.addParameter(QgsProcessingParameterFileDestination(self.OUTPUT_NETW, self.tr('Path to save Network object'), fileFilter = '.dat'))
+        self.addParameter(QgsProcessingParameterNumber(self.THRESHOLD, self.tr('Number of cells to initiate a channel'), defaultValue = 0))
+        self.addParameter(QgsProcessingParameterNumber(self.THETAREF, self.tr('m/n coeficient to calculate chi values in each channel cell'), QgsProcessingParameterNumber.Double, defaultValue = 0.45))
+        self.addParameter(QgsProcessingParameterBoolean(self.GRADIENTS, self.tr('Calculate gradients'), False, True))
         
-
     def processAlgorithm(self, parameters, context, feedback):
-        """
-        Here is where the processing itself takes place.
-        """
+        fd_raster = self.parameterAsRasterLayer(parameters, self.INPUT_FD, context)
+        netw_file = self.parameterAsFileOutput(parameters, self.OUTPUT_NETW, context)
+        threshold = self.parameterAsInt(parameters, self.THRESHOLD, context)
+        thetaref = self.parameterAsDouble(parameters, self.THETAREF, context)
+        gradients = self.parameterAsBool(parameters, self.GRADIENTS, context)
 
-        # Retrieve the feature source and sink. The 'dest_id' variable is used
-        # to uniquely identify the feature sink, and must be included in the
-        # dictionary returned by the processAlgorithm function.
-        source = self.parameterAsSource(
-            parameters,
-            self.INPUT,
-            context
-        )
+        fd = Flow(fd_raster.source()) #Load Flow Direction raster
+        netw = Network(fd, threshold = threshold, thetaref = thetaref, gradients = gradients)
+        netw.save(netw_file) #Save Network object onto disc
 
-        # If source was not found, throw an exception to indicate that the algorithm
-        # encountered a fatal error. The exception text can be any string, but in this
-        # case we use the pre-built invalidSourceError method to return a standard
-        # helper text for when a source cannot be evaluated
-        if source is None:
-            raise QgsProcessingException(self.invalidSourceError(parameters, self.INPUT))
-
-        (sink, dest_id) = self.parameterAsSink(
-            parameters,
-            self.OUTPUT,
-            context,
-            source.fields(),
-            source.wkbType(),
-            source.sourceCrs()
-        )
-
-        # Send some information to the user
-        feedback.pushInfo('CRS is {}'.format(source.sourceCrs().authid()))
-
-        # If sink was not created, throw an exception to indicate that the algorithm
-        # encountered a fatal error. The exception text can be any string, but in this
-        # case we use the pre-built invalidSinkError method to return a standard
-        # helper text for when a sink cannot be evaluated
-        if sink is None:
-            raise QgsProcessingException(self.invalidSinkError(parameters, self.OUTPUT))
-
-        # Compute the number of steps to display within the progress bar and
-        # get features from source
-        total = 100.0 / source.featureCount() if source.featureCount() else 0
-        features = source.getFeatures()
-
-        for current, feature in enumerate(features):
-            # Stop the algorithm if cancel button has been clicked
-            if feedback.isCanceled():
-                break
-
-            # Add a feature in the sink
-            sink.addFeature(feature, QgsFeatureSink.FastInsert)
-
-            # Update the progress bar
-            feedback.setProgress(int(current * total))
-
-        # To run another Processing algorithm as part of this algorithm, you can use
-        # processing.run(...). Make sure you pass the current context and feedback
-        # to processing.run to ensure that all temporary layer outputs are available
-        # to the executed algorithm, and that the executed algorithm can send feedback
-        # reports to the user (and correctly handle cancellation and progress reports!)
-        if False:
-            buffered_layer = processing.run("native:buffer", {
-                'INPUT': dest_id,
-                'DISTANCE': 1.5,
-                'SEGMENTS': 5,
-                'END_CAP_STYLE': 0,
-                'JOIN_STYLE': 0,
-                'MITER_LIMIT': 2,
-                'DISSOLVE': False,
-                'OUTPUT': 'memory:'
-            }, context=context, feedback=feedback)['OUTPUT']
-
-        # Return the results of the algorithm. In this case our only result is
-        # the feature sink which contains the processed features, but some
-        # algorithms may return multiple feature sinks, calculated numeric
-        # statistics, etc. These should all be included in the returned
-        # dictionary, with keys matching the feature corresponding parameter
-        # or output names.
-        return {self.OUTPUT: dest_id}
+        return {}
